b6e8a9b8a77889964913f7aac1368a792c5b9bf6
Nur Sasongko <32429721+ssasongko@users.noreply.github.com>
2020-10-15 21:47:08 +0700

    Add files via upload

---
+++ PBOIF2-10119049-Latihan15-Logika/src/pboif2/pkg10119049/latihan15/logika/PBOIF210119049Latihan15Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119049.latihan15.logika;
+
+/**
+ * @author 
+ * Nama  : Nur Sasongko
+ * Kelas : PBOIF2
+ * NIM   : 10119049
+ * Deskripsi Program : Latihan operator logika
+ */
+public class PBOIF210119049Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + (a && b));
+    }
+    
+}
